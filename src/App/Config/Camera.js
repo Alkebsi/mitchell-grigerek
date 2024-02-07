61c56429ced1287515bc544ad9b720ee595934ad
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2024-02-07 09:56:49 +0300

    Started working
    I am willing to make the room fell more like an isometric scene with a fake 2D vibe. I am also willing to add the city in one or more styles

--- src/App/Config/Camera.js
+++ src/App/Config/Camera.js
@@ -11,26 +11,43 @@ export default class Camera extends THREE.EventDispatcher {
     this.canvas = this.app.canvas;
     this.tests = this.app.tests;
 
-    this.setInstance();
-    this.setOrbitControls();
     if (this.tests.active) {
-      this.setTests();
+      this.setInstance(true);
+    } else {
+      this.setInstance(false);
     }
+    this.setOrbitControls();
   }
 
-  setInstance() {
-    this.instanceGroup = new THREE.Group();
-    this.instance = new THREE.PerspectiveCamera(
-      45,
-      this.sizes.width / this.sizes.height,
-      0.01,
-      100,
-    );
+  setInstance(test) {
+    if (!test) {
+      this.instanceGroup = new THREE.Group();
+      this.instance = new THREE.PerspectiveCamera(
+        45,
+        this.sizes.width / this.sizes.height,
+        0.01,
+        100,
+      );
 
-    this.instance.position.set(5, 4, 5);
-    this.instanceGroup.add(this.instance);
-    this.instanceGroup.position.set(0, 0, 0);
-    this.scene.add(this.instanceGroup);
+      this.instance.position.set(5, 4, 5);
+      this.instanceGroup.add(this.instance);
+      this.instanceGroup.position.set(0, 0, 0);
+      this.scene.add(this.instanceGroup);
+    } else {
+      this.tests.camera = this.tests.world.addFolder('Camera');
+      this.instanceGroup = new THREE.Group();
+      this.instance = new THREE.PerspectiveCamera(
+        45,
+        this.sizes.width / this.sizes.height,
+        0.01,
+        100,
+      );
+
+      this.instance.position.set(5, 4, 5);
+      this.instanceGroup.add(this.instance);
+      this.instanceGroup.position.set(0, 0, 0);
+      this.scene.add(this.instanceGroup);
+    }
   }
 
   setOrbitControls() {
@@ -42,6 +59,10 @@ export default class Camera extends THREE.EventDispatcher {
     this.controls.minDistance = 3;
     this.controls.maxDistance = 10;
     this.controls.enableDamping = true;
+
+    if (this.tests.active) {
+      this.setTests();
+    }
   }
 
   setTests() {
